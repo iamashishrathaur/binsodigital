62fca5d3c0f83b2254a71bb5e43f72df90f09185
Ashish Rathaur <121791024+iamashishrathaur@users.noreply.github.com>
2024-06-17 23:12:45 +0530

    make service page

---
+++ src/components/ServiceList.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+
+const services = [
+  {
+    id: 1,
+    icon: 'fa-shield-alt',
+    title: 'Cyber Security',
+    description: 'Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum dolore sed',
+    delay: '0.3s'
+  },
+  {
+    id: 2,
+    icon: 'fa-chart-pie',
+    title: 'Data Analytics',
+    description: 'Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum dolore sed',
+    delay: '0.6s'
+  },
+  {
+    id: 3,
+    icon: 'fa-code',
+    title: 'Web Development',
+    description: 'Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum dolore sed',
+    delay: '0.9s'
+  },
+  {
+    id: 4,
+    icon: 'fab fa-android',
+    title: 'Apps Development',
+    description: 'Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum dolore sed',
+    delay: '0.3s'
+  },
+  {
+    id: 5,
+    icon: 'fa-search',
+    title: 'SEO Optimization',
+    description: 'Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum dolore sed',
+    delay: '0.6s'
+  }
+];
+
+const ServiceList = () => {
+  return (
+    <section
+      id='service-list'
+      className="container-fluid py-5 wow fadeInUp service-block-two solutions"
+      data-wow-delay="0.1s"
+    >
+      <div className="container py-5">
+        <div className="sec-title text-center">
+          <span className="sub-title">WHAT WE’RE OFFERING</span>
+          <h2>
+            Get the best IT technology <br />
+            services <span>&amp;</span> solutions
+          </h2>
+        </div>
+        <div className="row gy-5">
+          {services.map((service) => (
+            <div key={service.id} className="col-lg-4 col-md-6 wow zoomIn"
+              data-wow-delay={service.delay}
+            >
+              <div className="inner-box service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
+                <div className="service-icon">
+                  <i className={`fa ${service.icon} text-white`} />
+                </div>
+                <h4 className="mb-3">{service.title}</h4>
+                <p className="m-0">
+                  {service.description}
+                </p>
+                <div className="button-box">
+                  <a href="page-service-details.html" className="read-more">
+                    Read More <i className="fa fa-angle-right"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+          <div className="col-lg-4 col-md-6 wow zoomIn"
+            data-wow-delay="0.9s"
+          >
+            <div className="position-relative bg-con-background rounded h-100 d-flex flex-column align-items-center justify-content-center text-center p-5">
+              <h3 className="text-white mb-3">Call Us For Quote</h3>
+              <p className="text-white mb-3">
+                Clita ipsum magna kasd rebum at ipsum amet dolor justo dolor est
+                magna stet eirmod
+              </p>
+              <h2 className="text-white mb-0">+012 345 6789</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ServiceList;
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,12 +1,14 @@
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import PageTittle from "../components/PageTittle"
+import ServiceList from "../components/ServiceList"
 
 const Services = () => {
   return (
     <>
     <Header/>
     <PageTittle name={'Services'}/>
+     <ServiceList/>
     <Footer/>
     </>
   )
